10ab8f5348b180d7595df1cb13cbc5d981837e99
gizemkulp <kulpluoglu.gizem@hotmail.com>
2023-04-24 02:54:23 +0300

    Mezuniyet-Projesi

--- proje.js
+++ proje.js
@@ -1,15 +1,16 @@
-var images = document.querySelectorAll('.slider img');
-var current = 0;
-var time = 3000;
+  var images = document.querySelectorAll('.slider img');
+  var current = 0;
+  var time = 3000;
 
-function slide() {
-  for (var i = 0; i < images.length; i++) {
-    images[i].classList.remove('active');
+  function slide() {
+    for (var i = 0; i < images.length; i++) {
+      images[i].classList.remove('active');
+    }
+    current = (current != images.length - 1) ? current + 1 : 0;
+    images[current].classList.add('active');
+    setTimeout(slide, time);
   }
-  current = (current != images.length - 1) ? current + 1 : 0;
-  images[current].classList.add('active');
-  setTimeout(slide, time);
-}
-slide();
+  slide();
 
 
+  
